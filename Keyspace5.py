36e2a6f2f9ccda661c305e7932f910b76a5241a5
dragonhlf8 <138527763+dragonhlf8@users.noreply.github.com>
2024-06-28 00:17:07 +0100

    Add files via upload

---
+++ Keyspace5.py
@@ -0,0 +1,37 @@
+# Keyspace Puzzle 64 Random
+# Made by Andrei Melek
+# https://github.com/xh0st/keyspace64
+
+import random
+import sys
+from bitcoin import *
+
+try:
+    import subprocess
+    subprocess.check_call(["python3", '-m', 'pip', 'install', 'bitcoin'])
+except ImportError: 
+    pass
+
+while True:  
+    low  = 0x100000000000000000
+    high = 0x1fffffffffffffffff
+    val = str(hex(random.randrange(low, high)))[2:]
+    result = val.rjust(46 + len(val), '0')
+    priv = result
+    pub = privtopub(priv)
+    pubkey1 = encode_pubkey(privtopub(priv), "bin_compressed")
+    addr = pubtoaddr(pubkey1)
+    n = addr
+    if n.startswith('19vkiEajfhuZ8bs8Zu2jgmC6oqZbWqh'):
+        print("found!!", addr, result)
+        k1 = priv
+        k2 = pub
+        k3 = addr
+
+        file = open('boom.txt', 'a')
+        file.write("Private key: " + k1 + '\n' + "Public key: " + k2 + '\n' + "Address: " + k3 + '\n\n')    
+        file.close()
+        break  # Add the break statement to exit the while loop
+    else:
+        sys.stdout.write('\r' + ' ' * 100 + '\r')  # Clear the previous line
+        print("searching...", addr, result, end='')
